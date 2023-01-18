e5f83144bc556caebd41d1e7b15ca2cab53f747b
micknice <mikemccullagh@outlook.com>
2023-01-18 22:42:30 +0000

    add push incorrectly classified to array, tests run, 96/99 correct with 200 bands, 94/99 correct with 21 bands

--- classifier.js
+++ classifier.js
@@ -10,7 +10,7 @@ linearModel = require('./linearmodel');
 //index for freq data
 const freqData = 2;
 //setting band number for fft
-const bands = 50;
+const bands = 48;
 
 //training sets
 const trainingSetKick = "C:\\Users\\NiceGuyMicky\\GIT REPOS\\wav-featurizer\\jaswavexperiment\\Kicks";
@@ -22,7 +22,7 @@ const testSet = "C:\\Users\\NiceGuyMicky\\GIT REPOS\\wav-featurizer\\jaswavexper
 
 const testArray = featurizedArrayGenerator(testSet, bands);
 // console.log(testArray)
-
+console.log(testArray[0][0])
 
 
 //linear models for each class(Kick/Snare/Hat)
@@ -42,37 +42,40 @@ modelHat.m = vectorAverage(featurizedArrayGenerator(trainingSetHat, bands));
 
 
 
-
+// classify by linear model
 let correctlyClassified = 0;
-for (testSample in testArray) {
-    
-    
+const incorrectlyClassified = [];
+for (testSample in testArray) {   
     let dotKick = modelKick.dotProduct(testArray[testSample][freqData], true);
     let dotSnare = modelSnare.dotProduct(testArray[testSample][freqData], true);
     let dotHat = modelHat.dotProduct(testArray[testSample][freqData], true);
     if (dotKick > dotSnare && dotKick > dotHat) {
-        if (testArray[testSample][1] === 'Kick') {
-            
+        if (testArray[testSample][1] === 'Kick') {            
             correctlyClassified += 1;
+        } else if (testArray[testSample][1] !== 'Kick') { 
+            incorrectlyClassified.push(testArray[testSample][0]);                      
         }
     } else if (dotSnare > dotKick && dotSnare > dotHat) {
-        if (testArray[testSample][1] === 'Snare') {
-            
+        if (testArray[testSample][1] === 'Snare') {            
             correctlyClassified += 1;
+        } else if (testArray[testSample][1] !== 'Snare') { 
+            incorrectlyClassified.push(testArray[testSample][0]);                      
         }          
     } else if (dotHat > dotKick && dotHat > dotSnare) {
-        if (testArray[testSample][1] === 'Hat') {
-            
+        if (testArray[testSample][1] === 'Hat') {           
             correctlyClassified += 1;
-        }    
+        } else if (testArray[testSample][1] !== 'Hat') { 
+            incorrectlyClassified.push(testArray[testSample][0]);                      
+        }   
     } else {
-    
+            
     }
 
 
 }
 console.log(correctlyClassified)
 console.log(testArray.length)
+console.log(incorrectlyClassified)
 
 
 
