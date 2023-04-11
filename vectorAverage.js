d596b70e8caf8ac7dbae9fccc6a4847747303153
micknice <107580574+micknice@users.noreply.github.com>
2023-04-11 05:04:02 +0100

    Update vectorAverage.js

--- vectorAverage.js
+++ vectorAverage.js
@@ -1,12 +1,10 @@
 const featurizedArrayGenerator = require('./featurizedArrayGenerator');
 
 // function takes featurized array and returns vector average 
-
 function vectorAverage(featurizedArray) {
     const freqData = 2;
     let vectorSum  = featurizedArray[0][freqData];
-    
-    
+      
     for (let i = 1; i < featurizedArray.length; i++) {
         for (let j = 0; j < vectorSum.length; j++) {
             vectorSum[j] += featurizedArray[i][freqData][j];
@@ -15,8 +13,7 @@ function vectorAverage(featurizedArray) {
     const vectorAve = [];
     for (let k = 0; k < vectorSum.length; k++) {
         vectorAve.push(vectorSum[k] / featurizedArray.length);
-    }
-    
+    }   
     return vectorAve;
 }
 
